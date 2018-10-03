4412f3dcc24ed48cb6b6aba0452213831639d390
IS Choi <insil321@hotmail.com>
2018-10-03 05:38:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+array=[]
 def oxford_comma(array)
-
+  if array.size ==1 
+    array[0]
+  else if array.size==2 
+   array.join(" and ")
+else 
+  array[0..-2].join(", ") + ", and " + array[-1]
 end
+end
+end
+
+   
+
+
+
+   
