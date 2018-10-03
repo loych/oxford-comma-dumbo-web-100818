3b365ad16540647345138a1c8de57477daaf411d
IS Choi <insil321@hotmail.com>
2018-10-03 05:37:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
   if array.size ==1 
     array[0]
   else if array.size==2 
-   array.join(" and ")
+   array.join(", and ")
 else 
-  array[0..-2].join(" , ") + " and " + array[-1]
+  array[0..-2].join(", ") + " and " + array[-1]
 end
 end
 end
