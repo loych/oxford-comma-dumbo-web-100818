af6fd941681bf6a635010e16d97031a27619d3b7
IS Choi <insil321@hotmail.com>
2018-10-03 05:38:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,20 @@ def oxford_comma(array)
   if array.size ==1 
     array[0]
   else if array.size==2 
-   array.join(", and ")
+   array.join(" and ")
 else 
-  array[0..-2].join(", ") + " and " + array[-1]
+  array[0..-2].join(", ") + ", and " + array[-1]
 end
 end
 end
 
    
+
+
+
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
